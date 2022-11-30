3838fef1c99986d373f110f6d3c0ee5181212914
Cierra Lovelace <cece@Cierras-MacBook-Air.local>
2022-11-29 21:33:36 -0800

    initial commit

---
+++ src/PhoneBookMain.java
@@ -0,0 +1,208 @@
+
+
+import java.util.*;
+public class PhoneBookMain {
+
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        PhoneBookManager javaPages = new PhoneBookManager();
+
+        // Load generic phone book
+        loadTestNodes(javaPages);
+
+        // An intro message is displayed on program start
+        displayIntroMessage();
+
+        String command; // input variable
+        boolean done = false; //  boolean flag
+        do {
+            displayMenu();
+            command = s.nextLine().toLowerCase();  // accept an input command
+            // user command options 
+            switch (command) {
+                case "a":
+                    addEntry(s, javaPages);
+                    break;
+                case "m":
+                    modifyEntry(s, javaPages);
+                    break;
+                case "d":
+                    displayList(javaPages);
+                    break;
+                case "b":
+                    displayBellingham(javaPages);
+                    break;
+                case "s":
+                    displaySeattle(javaPages);
+                    break;
+                case "q":
+                    done = true;
+                    break;
+                default: // valid input commands not found
+                    System.out.println("Invalid command. Select from the commands above.");
+                    break;
+            }
+        } while (!done); // end of do while loop 
+    }
+
+    // introduction to the program
+    public static void displayIntroMessage() {
+        System.out.println("============================================");
+        System.out.println("===== WELCOME TO YOUR LOCAL JAVA PAGES =====");
+        System.out.println("============================================");
+        System.out.println("==== Your directory of all that matters ====");
+        System.out.println("============================================");
+        System.out.println();
+    }
+
+    // displays the user menu
+    public static void displayMenu() {
+        System.out.println();
+        System.out.print("(A)dd a new entry \n(M)odify an existing entry \n(D)isplay complete phone book list " +
+                "\n(B)ellingham phone book list \n(S)eattle phone book list \n(Q)uit \n");
+        System.out.println();
+        System.out.print("Enter a command from the list above: ");
+    }
+
+    // method to add a phone book entry
+    public static void addEntry(Scanner s, PhoneBookManager javaPages) {
+        System.out.println();
+        System.out.println("Adding an entry:");
+        System.out.print("Enter a FIRST NAME: ");
+        String firstName = s.nextLine();
+        System.out.print("Enter a LAST NAME: ");
+        String lastName = s.nextLine();
+        System.out.print("Enter an ADDRESS: ");
+        String address = s.nextLine();
+        System.out.print("Enter a CITY: ");
+        String city = s.nextLine();
+        System.out.print("Enter a PHONE NUMBER: ");
+        String phoneNumber = s.nextLine();
+        System.out.print("Enter an EMAIL ADDRESS: ");
+        String emailAddress = s.nextLine();
+        System.out.println("--Entry created--");
+        PhoneBookNode newEntry = new PhoneBookNode(firstName, lastName, address, city, phoneNumber, emailAddress);
+        javaPages.append(newEntry);
+    }
+
+    // method to modify a phone book entry
+    public static void modifyEntry(Scanner s, PhoneBookManager javaPages) {
+        String command;
+        boolean done = false;// define boolean for do while loop
+        displayList(javaPages);
+        System.out.println();
+        System.out.print("Enter the number of the entry to modify: ");
+        System.out.println();
+        int index = s.nextInt();
+        // loop to re-prompt and check for user input if an invalid value is given
+        while (index < 1 || index > javaPages.size()) {
+            System.out.println("Not a valid index! Try again...");
+            index = s.nextInt();
+        }
+        PhoneBookNode current = new PhoneBookNode(); 
+        current.next = javaPages.head;
+        // user option menu 
+        System.out.print("(D)elete the entry or (M)odify a field?");
+        String command2;
+        do { // beginning of do while loop to continuously run through menu
+            command2 = s.nextLine().toLowerCase();
+            switch (command2) {
+                case "d":
+                    if (index == 1) {
+                        javaPages.head = javaPages.head.next;
+                        done = true;
+                        return;
+                    } else {
+                        for (int i = 0; i < index - 1; i++) {
+                            current = current.next;
+                        }
+                        current.next = current.next.next;
+                        done = true;
+                        return;
+                    }
+                    //break;
+                case "m": // revert to menu 
+
+                    for (int i = 0; i < index; i++) {
+
+                        current = current.next;
+                    }
+                    // user menu for modifications 
+                    System.out.println("(F)irst name, (L)ast name, (A)ddress, (C)ity, (P)hone number, (E)mail address");
+                    System.out.println("Select a field to modify:  ");
+
+                    command = s.nextLine().toLowerCase();  // accept an input command
+                    // user modification commands 
+                    switch (command) {
+                        case "f":
+                            System.out.print("New first name:");
+                            String firstName = s.nextLine();
+                            current.setFirstName(firstName);
+                            break;
+                        case "l":
+                            System.out.println("New last name:");
+                            String lastName = s.nextLine();
+                            current.setLastName(lastName);
+                            break;
+                        case "a":
+                            System.out.println("New address:");
+                            String address = s.nextLine();
+                            current.setAddress(address);
+                            break;
+                        case "c":
+                            System.out.println("New city:");
+                            String city = s.nextLine();
+                            current.setCity(city);
+                            break;
+                        case "p":
+                            System.out.println("New phone number:");
+                            String phoneNumber = s.nextLine();
+                            current.setPhoneNumber(phoneNumber);
+                            break;
+                        case "e":
+                            System.out.println("New email address:");
+                            String emailAddress = s.nextLine();
+                            current.setEmailAddress(emailAddress);
+                            break;
+                        default: // valid input commands not found
+                            System.out.println("Invalid command. Select from the commands above.");
+                            break;
+                    }
+                    done = true;
+                    break;
+                }
+                //done = true;
+        } while (!done);
+    }
+
+    // print the entire phone book
+    public static void displayList(PhoneBookManager javaPages) {
+        javaPages.printList();
+    }
+    // search the phone book list and print the entries with Bellingham in the city field
+    public static void displayBellingham(PhoneBookManager javaPages) {
+        System.out.println();
+        System.out.println("===Bellingham Phone Book===");
+        javaPages.searchList("city", "Bellingham");
+    }
+
+    // search the phone book list and print the entries with Seattle in the city field
+    public static void displaySeattle(PhoneBookManager javaPages) {
+        System.out.println();
+        System.out.println("===Seattle Phone Book===");
+        javaPages.searchList("city", "Seattle");
+    }
+
+    // given a PhoneBookManager list, loads test nodes
+    public static void loadTestNodes(PhoneBookManager javaPages) {
+        PhoneBookNode person1 = new PhoneBookNode("Joe", "Schmoe", "123 Coal Valley Ln", "Bellingham", "360-555-1212", "yes@no.com");
+        javaPages.append(person1);
+        PhoneBookNode person2 = new PhoneBookNode("Jane", "Doe", "321 Gold Hill Rd", "Seattle", "555-555-1212", "no@yes.com");
+        javaPages.append(person2);
+        PhoneBookNode person3 = new PhoneBookNode("Darrell", "Criss", "237 W Kellogg Rd", "Bellingham", "360-393-3739", "dcriss@whatcom.edu");
+        javaPages.append(person3);
+        PhoneBookNode person4 = new PhoneBookNode("Ding", "Dong", "111 Northgate Wy", "Seattle", "250-393-3739", "hello@hi.com");
+        javaPages.append(person4);
+    }
+}
---
+++ src/PhoneBookManager.java
@@ -0,0 +1,145 @@
+
+
+public class PhoneBookManager {
+    PhoneBookNode head;
+
+    // given a PhoneBookNode, adds it to the end of the list
+    public void append(PhoneBookNode node) {
+        if (head == null) { // no nodes in the list yet
+            head = node;  // this node becomes the head of the list
+            return;
+        }
+        PhoneBookNode current = head;
+        while (current.next != null) {
+            current = current.next;
+        }
+        current.next =  node;  // adding this node to the end of the list
+        head = sortList(head);
+    }
+
+    // display all the phone book entries
+    public void printList() {
+        if (head == null) { // no nodes in the list
+            System.out.println("!! No entries to display. Please add a new entry.");
+            return;
+        }
+        PhoneBookNode current = head;
+        int index = 1;  // a reference number for each entry
+        System.out.println();
+        System.out.printf("%3d. ", index);
+        System.out.println(current);
+        while (current.next != null) {
+            current = current.next;
+            index++;
+            System.out.printf("%3d. ", index);
+            System.out.println(current);
+        }
+    }
+
+    // given a field name and search string, print out the matching phone book entries
+    public void searchList(String field, String value) {
+        if (head == null) { // no nodes in the list
+            System.out.println("!! No entries to display. Please add a new entry.");
+            return;
+        }
+        PhoneBookNode current = head;
+        while (current != null) {
+            if (search(current, field, value)) {
+                System.out.println(current);
+            }
+            current = current.next;
+        }
+    }
+
+    // given a node, field name and search string,
+    // returns true if the string is found in the field
+    public boolean search(PhoneBookNode node, String field, String value) {
+        boolean found = false;
+        // match the search string against the string in the field
+        switch (field) {
+            case "firstName":
+                return (node.getFirstName().equalsIgnoreCase(value));
+            case "lastName":
+                return (node.getLastName().equalsIgnoreCase(value));
+            case "address":
+                return (node.getAddress().equalsIgnoreCase(value));
+            case "city":
+                return (node.getCity().equalsIgnoreCase(value));
+            case "phoneNumber":
+                return (node.getPhoneNumber().equalsIgnoreCase(value));
+            case "emailAddress":
+                return (node.getEmailAddress().equalsIgnoreCase(value));
+        }
+        return found;
+    }
+
+    // implementation of merge sort algorithm
+    // given a head node, returns the head node of a sorted list
+    public PhoneBookNode sortList(PhoneBookNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        PhoneBookNode temp = head;
+        PhoneBookNode slow = head;
+        PhoneBookNode fast = head;
+
+        // loop to find the middle node of the list
+        // using a fast and slow pointer
+        while (fast != null && fast.next != null) {
+            temp = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        temp.next = null; // breaking the list into 2 at temp
+
+        // recursively break down into smaller lists
+        PhoneBookNode left = sortList(head);
+        PhoneBookNode right = sortList(slow);
+
+        return merge(left, right);
+    }
+
+    public PhoneBookNode merge(PhoneBookNode list1, PhoneBookNode list2) {
+
+        PhoneBookNode result;
+
+
+        // base cases of recursive function
+        if (list1 == null) {
+            return list2;
+        }
+        if (list2 == null) {
+            return list1;
+        }
+
+        // sort by last name
+        if (list1.getLastName().compareToIgnoreCase(list2.getLastName()) <= 0) {
+            result = list1;
+            result.next = merge(list1.next, list2);
+        } else {
+            result = list2;
+            result.next = merge(list1, list2.next);
+        }
+
+        return result;
+    }
+
+    public int size() {
+        PhoneBookNode current = head;
+        if (current == null) {
+            return 0;
+        } else if (current.next == null) {
+            return 1;
+        } else {
+            int count = 1;
+            while (current.next != null) {
+                count++;
+                current = current.next;
+            }
+            return count;
+        }
+    }
+
+}
